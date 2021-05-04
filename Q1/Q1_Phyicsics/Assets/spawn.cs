37b97384fa53f577bf02b55b5ce3fd69ad75375e
Opity69 <68939387+Opity69@users.noreply.github.com>
2021-05-04 17:52:29 +0800

    Q1 done

---
+++ Q1/Q1_Phyicsics/Assets/spawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject obj;
+    public int amount;
+
+    void Start()
+    {
+
+        
+        for (int i = 0; i < amount; i++) {
+
+
+          Instantiate(obj, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+           
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
