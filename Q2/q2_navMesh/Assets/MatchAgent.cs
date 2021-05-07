96381fb4fc920195e94ca3d4e7f927c343a36211
Opity69 <lucasob11@gmail.com>
2021-05-07 11:02:08 +0800

    Q2  done

---
+++ Q2/q2_navMesh/Assets/MatchAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MatchAgent : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public NavMeshAgent agent007;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.transform.position = agent007.transform.position;
+
+
+    }
+}
---
+++ Q2/q2_navMesh/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Camera Cam;
+    public NavMeshAgent agent007;
+    void Start()
+    {
+        Cursor.visible = true;
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        { 
+            Debug.Log("Update");
+            Ray Camray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camray, out hit))
+            {
+                agent007.SetDestination(hit.point);
+            }
+        }
+
+    }
+}
